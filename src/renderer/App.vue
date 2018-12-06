49b8ecf668bc7443edbcf137690de6ac171bfc91
xiangdeshan <xiangdeshan_120@sina.com>
2018-12-06 10:26:56 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'gf6mes',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
